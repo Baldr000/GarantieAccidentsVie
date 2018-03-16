bebdccf5a52abb65283ebb02c2593f02cb248ca0
marcMunier <marc.munier@insa-cvl.fr>
2018-03-16 01:41:03 +0100

    Rien

--- GitPush.sh
+++ GitPush.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-
+if [ -n $1 ] 
+then 
+	print "Qu'est ce que tu as fait ?"
+	read -r commit
+else
+	commit=$1
+fi
 
 git add *
-git commit -m "What are you doing"
+git commit -m $commit
 #git remote add origin https://github.com/Baldr000/GarantieAccidentsVie.git
 git push -u origin master
